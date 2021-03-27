319436a30c37f11ae0ddd5340e07cc3f8afb24eb
Philip <philip.henderson@hotmail.com>
2021-03-27 00:08:16 -0400

    Test

---
+++ src/Condition.h
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef __CONDITION__
+#define __CONDITION__
+
+class Condition
+{
+public:
+	Condition(bool condition = false);
+	~Condition();
+
+	virtual bool Test();
+	void SetCondition(bool condition);
+private:
+	bool m_condition;
+};
+
+
+
+
+#endif /*(__CONDITION__)*/
